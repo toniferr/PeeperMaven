3d80bee08caaa32c3903db347005738d58fe2c00
toni <ferreirocouto@gmail.com>
2020-06-24 14:50:12 +0100

    initial commit

---
+++ src/main/java/com/toni/PeeperMavenMain.java
@@ -0,0 +1,11 @@
+package com.toni;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class PeeperMavenMain {
+
+	public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("PeeperMaven"));
+	}
+
+}
