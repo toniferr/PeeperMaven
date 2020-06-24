50232cac7b1ac65b5f07ae5336a044b7daad3ffc
toni <ferreirocouto@gmail.com>
2020-06-24 18:53:21 +0100

    actualizacion test clase para maven surefire plkugin

--- src/test/java/com/toni/PeeperMavenMainTest.java
+++ src/test/java/com/toni/PeeperMavenMainTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class PeeperMavenTestMain {
+public class PeeperMavenMainTest {
 	
 	@Test
     public void testMaven(){
