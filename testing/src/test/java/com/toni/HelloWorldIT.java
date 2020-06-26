217021a91010af989857271a92d4a55ec9ae1449
toni <ferreirocouto@gmail.com>
2020-06-26 18:32:10 +0100

    añadido test IT con sailsafe

---
+++ testing/src/test/java/com/toni/HelloWorldIT.java
@@ -0,0 +1,12 @@
+package com.toni;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldIT {
+
+    @Test
+    void integrationTest() {
+
+        System.out.println("integration test");
+    }
+}
